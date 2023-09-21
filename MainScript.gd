052fd3d6a6765748a47be4dfd3b8e12bfc4f1fc2
PZ991 <pureanimationz@gmail.com>
2023-09-21 15:19:50 +0800

    rigidbody

--- MainScript.gd
+++ MainScript.gd
@@ -1,6 +1,8 @@
-extends Node
+extends Node3D
 
-
+class_name MainScript
+var speed: float = 1000
+var jumpforce: float= 2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +10,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if $RigidBody3D:
+		var childrb=$RigidBody3D
+	
+		if(Input.is_key_pressed(KEY_W)):
+			$RigidBody3D.apply_force(Vector3(0,0,speed)/Engine.get_frames_per_second())
+		if(Input.is_key_pressed(KEY_S)):
+			$RigidBody3D.apply_force(Vector3(0,0,-speed)/Engine.get_frames_per_second())
+		if(Input.is_key_pressed(KEY_A)):
+			$RigidBody3D.apply_force(Vector3(-speed,0,0)/Engine.get_frames_per_second())
+		if(Input.is_key_pressed(KEY_D)):
+			$RigidBody3D.apply_force(Vector3(speed,0,0)/Engine.get_frames_per_second())
+		if(Input.is_key_pressed(KEY_SPACE)):
+			childrb.apply_impulse(Vector3(0,jumpforce,0))
+	
+pass
